1c02f2ed5d14727d14fb15481b2bdd9ef3d8ad55
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2022-02-24 17:24:41 -0300

    variaveis

---
+++ Pacote 2 - Variaveis/variaveis.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variavel1 string = "teste"
+	fmt.Println(variavel1)
+	var variavel2 = 123
+	fmt.Println((variavel2))
+	variavel3 := "variavel de teste"
+	fmt.Println((variavel3))
+	const variavel4 = "variavel de teste 2"
+	fmt.Println((variavel4))
+}
