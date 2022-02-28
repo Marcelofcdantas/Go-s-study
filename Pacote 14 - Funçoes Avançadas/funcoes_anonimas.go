af545c4fc93acc3db3882302090ef4d6bc69d7ac
marcelofcdantas <marcelofcd@gmail.com>
2022-02-27 22:57:02 -0300

    funcoes anonimas

---
+++ Pacote 14 - Funçoes Avançadas/funcoes_anonimas.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// func () {
+	// 	fmt.Println("Ola mundo")
+	// }()
+	retorno:= func (texto string) string {
+		return fmt.Sprintf("Ola mundo do %s", texto)
+	}("parametro")
+
+	fmt.Println(retorno)
+}
